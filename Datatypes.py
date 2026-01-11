5e153c82289b5662e2277f29d040d81e0a46d3fd
Jagritidas123 <jagritid764@gmail.com>
2026-01-11 19:01:07 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,4 @@
+# DATATYPE are the type of values we store in the variable
+# INTERGER , FLOAT, COMPLEX = imaginary number , e.g   a= 34j
+# STRING , we have to use "" or ''
+# BOOLEAN give the result as true pr false
