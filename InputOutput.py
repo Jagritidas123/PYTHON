72eb8e41a66dc74fac6779b09fc136f690652f16
Jagritidas123 <jagritid764@gmail.com>
2026-01-11 19:02:00 +0530

    Add files via upload

---
+++ InputOutput.py
@@ -0,0 +1,14 @@
+# print() is the only way for the output
+
+
+name ="Riya"
+age = 10
+print("My age is ", name , "my age is ", age)
+
+# Formatted string
+print(f"my name is {name} and my age is {age}")
+
+# INPUT
+
+age = input("What is your age ? ")
+print(f"my age is {age}")
