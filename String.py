fd46ca8da228ee33340d80ea42e40a81b479c793
Jagritidas123 <jagritid764@gmail.com>
2026-01-11 19:01:28 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,11 @@
+a = "A"
+print(ord(a))
+# THIS IS FOR CHECKING THE UNICODE
+
+
+# STRING INDEXING= starts from 0 (Positive indexing) -1 (negatie indexing)
+x = "Rainbow"
+print(x[5]) 
+# positive indexing
+print(x[-1])
+# negative indexing
