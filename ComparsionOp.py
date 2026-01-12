05317d9e1062a4dc4ab361074e26d7f3b83f3559
Jagritidas123 <jagritid764@gmail.com>
2026-01-12 16:00:55 +0530

    Add files via upload

---
+++ ComparsionOp.py
@@ -0,0 +1,13 @@
+# Comparing between two  , you will get the results in Boolean
+a = 10
+b = 20
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print(a>b)
+print(a<b)
+# We can use this in string as well
+print(ord("A"))
+print(ord("B"))
+print("A" > "B")
