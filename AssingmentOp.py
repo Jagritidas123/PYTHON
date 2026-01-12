13f4b55ff49151d0683eb996b1895c9a66e2f87d
Jagritidas123 <jagritid764@gmail.com>
2026-01-12 16:00:36 +0530

    Add files via upload

---
+++ AssingmentOp.py
@@ -0,0 +1,9 @@
+# You craete a variable and it assign the value to it
+# Compund assignment oparations
+a = 20
+a = a + 20
+a = a + 40
+a += 20
+# We can reassign values in python
+# We can do similar things with -, *,/
+print(a)
