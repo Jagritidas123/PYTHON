a74b68f923197a1f282a3eeb2a2ecc8349617248
Jagritidas123 <jagritid764@gmail.com>
2026-01-12 16:01:13 +0530

    Add files via upload

---
+++ LogicalOp.py
@@ -0,0 +1,14 @@
+# Two or more comparision can work together
+"""
+AND = Return true if both conditions are true
+OR = Return true if at least one condition is true
+NOT = Reverse the boolean value
+"""
+
+print( 10 < 20 and 20 < 30)
+print( 10 < 20 and 20 > 30)
+print( 10 < 20 or 20 > 30)
+print(not  10 < 20)
+
+
+
